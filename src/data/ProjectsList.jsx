1e35f8fa426e4c7ccbed1bef33d5786a4353c853
Ardox <ardox@axos-project.com>
2024-10-08 21:15:03 +0200

    Initial commit

---
+++ src/data/ProjectsList.jsx
@@ -0,0 +1,123 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "AxOS",
+      link: "https://axos-project.com",
+      description:
+        "Step into a world where innovation meets functionality; welcome to AxOS, the pinnacle of operating system excellence. AxOS isn't just a platform; it's a revolution in the way we interact with technology. Imagine a landscape where your digital interactions are seamless and intuitive. AxOS brings this vision to life through its state-of-the-art architecture, blending cutting-edge technology with user-centric design. Every click, every swipe, and every task is meticulously crafted to enhance your computing experience.",
+      technologies: [
+        "Linux",
+        "Bash",
+        "Lua",
+        "Javascript",
+        "AxMirrors",
+        "Git",
+        "My brain",
+        "GitHub",
+      ],
+    },
+    {
+      name: "Oriana",
+      link: "",
+      description:
+        "Oriana is a virtual assistant powered by the Ariax 7b model, my own model based Mistral and Llava. Oriana can chat, replicate and analyze emotions, read documents and analyse images, browse internet, interact with my system and other cool features. ",
+      technologies: [
+        "Python",
+        "PyTorch",
+        "Hugging Face",
+        "Promt Engineering",
+      ],
+    },
+    {
+      name: "Personal Portfolio",
+      link: "/",
+      description:
+        "My portfolio is a website built with React.js and Tailwind CSS, utilizing React Router v6 to enable easy access to multiple pages. It was designed and created entirely by myself, showcasing my skills and projects. The website is responsive and visually appealing, providing a user-friendly experience.",
+      technologies: ["React JS", "Tailwind CSS", "Git", "GitHub"],
+    },
+    {
+      name: "JFCocktails",
+      link: "https://github.com/Mouuxime/JFCocktails",
+      description:
+        "Jfcocktails is my final school project of 2024. I made a completely functional and autonomous cocktail machine. The clients can order different drinks using the touchscreen on the machine or his mobile app. Every recipies are stocked in a mysql database. ",
+      technologies: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "React JS",
+        "Node-Red",
+        "MySQL",
+        "Git",
+        "GitHub",
+      ],
+    },
+    {
+      name: "Dots-Hyprland",
+      link: "https://github.com/LeVraiArdox/dots-hyprland",
+      description:
+        "I developed a modern and highly customizable Hyprland desktop environment, designed for usability and visual appeal. Featuring AI assistant integration, dynamic color schemes, and an intuitive overview widget, this configuration optimizes both functionality and style. I used Hyprland as the compositor and incorporated various utilities like AGS for widget control. The setup ensures a smooth, personalized experience while being lightweight and open-source.",
+      technologies: [
+        "Linux",
+        "Hyprland", 
+        "JavaScript",
+        "AGS",
+        "Git",
+        "GitHub",
+        "SCSS",
+      ],
+    },
+    {
+      name: "SchoolNotes",
+      link: "https://github.com/LeVraiArdox/SchoolNotes",
+      description:
+        "I created 'SchoolNotes,' a personal note archive platform containing my organized notes across various subjects i studied at school. This GitHub-hosted collection is designed for easy access and reference, featuring clear structure and formatting to support both study, note taking and revision. The notes span multiple topics, making it a valuable resource for academic progress.",
+      technologies: [
+        "React JS",
+        "MDX",
+        "Markdown",
+        "CSS",
+        "Git",
+        "GitHub",
+        "Vercel",
+      ],
+    },
+    {
+      name: "Fakebat",
+      link: "https://github.com/LeVraiArdox/fakebat",
+      description:
+        "Fakebat (fake-battery) is a kernel module for simulating one or multiple batteries on Linux. I use it for testing puposes on AxOS.",
+      technologies: [
+        "Linux",
+        "C",
+        "Makefile",
+        "Git",
+        "GitHub",
+      ],
+    },
+    {
+      name: "Discord-RPC",
+      link: "https://github.com/LeVraiArdox/discord-rpc",
+      description:
+        "I created a Discord Rich Presence integration for AxOS, allowing users to display their current activity on Discord. This feature enhances user experience by providing real-time status updates and activity tracking. The integration is lightweight and user-friendly, seamlessly integrating with the Discord platform.",
+      technologies: [
+        "JavaScript",
+        "Node JS",
+        "AxOS",
+        "Discord API",
+        "Git",
+      ],
+    },
+    {
+      name: "Ginger Archive",
+      link: "https://github.com/LeVraiArdox/ginger-archive",
+      description:
+        "Ginger Archive is a web-based platform i made for my high school engineering teacher. It is designed to store and manage PDF documents. The platform offers a secure and organized environment for users to store their data, with features such as search functionality, file preview, and file management. Ginger Archive is built using React.js, ensuring a seamless and user-friendly experience.",
+      technologies: [
+        "JavaScript",
+        "Node JS",
+        "React JS",
+      ],
+    },
+    
+  ],
+};
---
+++ src/data/UserData.jsx
@@ -0,0 +1,34 @@
+export const UserData = {
+  name: "Adrian",
+  about : " I'm a french Linux architect and self-taught developer known as Ardox. I'm the founder of AxOS and other cool projects. I enjoy learning about a variety of topics, especially artificial intelligence. ",
+  resumeUrl : "",
+  FooterLink : "https://github.com/levraiardox/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/levraiardox",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://fr.linkedin.com/in/adrian-arjoca-13a0552a4/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "https://instagram.com/lvardox",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Linux Architect",
+      "AI Enthusiast",
+      "Computer Science Student",
+      "Multi-lingual developer",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
