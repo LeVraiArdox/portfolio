e0478022fb67065885c71fdd365596bcd47ecc8c
Ardox <ardox@axos-project.com>
2024-11-20 21:57:06 +0100

    Full redisign

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+import type { Site, Links } from "@/types";
+
+export const SITE: Site = {
+  TITLE: "ARDOX",
+  DESCRIPTION: "Ardox's personal website",
+  AUTHOR: "Ardox",
+  REPO_URL: "https://github.com/levraiardox/",
+};
+
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "About",
+    HREF: "/about",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+];
