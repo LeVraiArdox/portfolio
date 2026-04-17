316a8039d0ae1b9028de550dd2f1d3ccde3264cb
Ardox <ardox@axos-project.com>
2026-04-17 13:56:04 +0200

    Update github to gitlab

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   TITLE: "Ardox",
   DESCRIPTION: "Ardox's personal website",
   AUTHOR: "Ardox",
-  REPO_URL: "https://github.com/levraiardox/",
+  REPO_URL: "https://gitlab.com/lvardox/",
 };
 
 export const LINKS: Links = [
